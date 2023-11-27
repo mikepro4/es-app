08943592f2a03e926ad88864b96e105650cdbafe
mikepro4 <mikhailpro4@gmail.com>
2023-11-26 21:26:12 -0500

    page setup

--- pages/genesis/algoPage.js
+++ pages/genesis/algoPage.js
@@ -3,7 +3,13 @@ import { useSelector, useDispatch} from "react-redux";
 import { useRouter } from 'next/router';
 import classNames from "classnames";
 
+import { Formik, Form, Field, FieldArray } from 'formik';
+import Input from "@/components/form/BladeInput";
 import Button from "@/components/button";
+
+import AlgoActionsView from "@/components/collection_actions/algoActions";
+
+import { algoUpdateItem, updateCollectionItem, algoItem} from "@/redux";
 
 function AlgoPageContainer({
     item
@@ -11,15 +17,56 @@ function AlgoPageContainer({
     const [loading, setLoading] = useState(false);
     const app = useSelector((state) => state.app);
     const router = useRouter();
+    const dispatch = useDispatch();
+
+    const [algo, setAlgo] = useState(false);
+
+    const fetchItem = () => {
+        dispatch(algoItem({
+            id: router.query.algoId,
+            callback: (data) => {
+                console.log(data);
+                setAlgo(data)
+                dispatch(updateCollectionItem(null))
+            }
+        }))
+    }
 
 
 
     useEffect(() => {
+        fetchItem()
 
         return () => {
             
         };
     }, []); 
+
+    useEffect(() => {
+        if(app.updateCollectionItem == algo?._id)  {
+            fetchItem()
+        }
+  
+    }, [app.updateCollectionItem]); 
+
+    const renderAlgo = () => {
+        if(algo && algo._id) {
+            return (
+                <div className="algo-page-header">
+                    <div className="algo-page-header-left">
+                        <h1>{algo.name}</h1>
+                    </div>
+
+                    <div className="algo-page-header-right">
+                        <AlgoActionsView
+                            item={algo}
+                        />
+                    </div>
+                    
+                </div>
+            )
+        }
+    }
 
     return (
         <div className="algo-page-container">
@@ -40,11 +87,14 @@ function AlgoPageContainer({
                     }}
                 />
 
-                <div className="algo-page-form">
-                    Algo page
+                {renderAlgo()}
+                
+                    
+                <div className="algo-preview"></div>
 
-                    <div className="placeholder"></div>
-                </div>
+
+
+                <div className="placeholder"></div>
 
 
             </div>
