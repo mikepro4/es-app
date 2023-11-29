1282499ad392ea447b83390144635df1ea6c2a8a
anmst21 <anmstudios21c@gmail.com>
2023-11-28 20:28:31 -0500

    nft working
    lallala

--- pages/mint/index.js
+++ pages/mint/index.js
@@ -1,13 +1,142 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { useState } from "react";
+import {
+  useActiveClaimConditionForWallet,
+  useClaimIneligibilityReasons,
+  useAddress,
+  useContract,
+  useContractMetadata,
+  useTotalCirculatingSupply,
+  useTotalCount,
+  MediaRenderer,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+import { TEST_CONTRACT_ADDRESS } from "@/config";
 
 export default function Genesis() {
+  const address = useAddress();
+
+  const { contract } = useContract(TEST_CONTRACT_ADDRESS);
+
+  const { data: contractMetadata, isLoading: isContractMetadataLoading } =
+    useContractMetadata(contract);
+
+  const { data: activeClaimPhase, isLoading: isActiveClaimPhaseLoading } =
+    useActiveClaimConditionForWallet(contract, address);
+
+  const { data: totalSupply, isLoading: isTotalSupplyLoading } =
+    useTotalCount(contract);
+
+  const { data: totalClaimed, isLoading: isTotalClaimedLoading } =
+    useTotalCirculatingSupply(contract);
+
+  const [claimQuantity, setClaimQuantity] = useState(1);
+
+  const increment = () => {
+    if (claimQuantity < maxClaimable) {
+      setClaimQuantity(claimQuantity + 1);
+    }
+  };
+  const decrement = () => {
+    if (claimQuantity > 1) {
+      setClaimQuantity(claimQuantity - 1);
+    }
+  };
+
+  const { data: claimIneleigibility, isLoading: isClaimIneleigibilityLoading } =
+    useClaimIneligibilityReasons(contract, {
+      walletAddress: address || "",
+      quantity: claimQuantity,
+    });
+
+  const maxClaimable = parseInt(activeClaimPhase?.maxClaimablePerWallet || "0");
+
   return (
     <>
       <div className="ui-screen">
         Mint
-
-        <div className="placeholder"></div>
+        <div className="placeholder">
+          {!isContractMetadataLoading && (
+            <div className="mint__heroSection">
+              <div className="mint__collectionImage">
+                <MediaRenderer src={contractMetadata?.image} />
+              </div>
+              <div className="mint__">
+                <h1>{contractMetadata.name}</h1>
+                <p>{contractMetadata.description}</p>
+                {!isActiveClaimPhaseLoading ? (
+                  <div>
+                    <p>Claim Phase: {activeClaimPhase?.metadata?.name}</p>
+                    <p>
+                      Price: {ethers.utils.formatUnits(activeClaimPhase?.price)}
+                    </p>
+                  </div>
+                ) : (
+                  <p>Loading...</p>
+                )}
+                {!isTotalClaimedLoading && !isTotalClaimedLoading ? (
+                  <p>
+                    Claimed: {totalClaimed?.toNumber()} /{" "}
+                    {totalSupply?.toNumber()}
+                  </p>
+                ) : (
+                  <p>Loading... </p>
+                )}
+                {address ? (
+                  !isClaimIneleigibilityLoading ? (
+                    claimIneleigibility?.length > 0 ? (
+                      claimIneleigibility.map((reason, index) => (
+                        <p key={index}>{reason}</p>
+                      ))
+                    ) : (
+                      <div>
+                        <p>
+                          Tou are eligible to claim.{" "}
+                          {`(Max claimable: ${maxClaimable})`}
+                        </p>
+                        <div className="mint__claimContainer">
+                          <div className="mint__claimContainer--claimValue">
+                            <button
+                              onClick={decrement}
+                              className="mint__claimContainer--claimBtn"
+                            >
+                              -
+                            </button>
+                            <input
+                              className="mint__claimContainer--claimInput"
+                              type="number"
+                              value={claimQuantity}
+                            />
+                            <button
+                              onClick={increment}
+                              className="mint__claimContainer--claimBtn"
+                            >
+                              +
+                            </button>
+                          </div>
+                        </div>
+                        <Web3Button
+                          contractAddress={TEST_CONTRACT_ADDRESS}
+                          action={(contract) =>
+                            contract.erc721.claim(claimQuantity)
+                          }
+                        >
+                          Claim NFT
+                        </Web3Button>
+                      </div>
+                    )
+                  ) : (
+                    <p>Loading...</p>
+                  )
+                ) : (
+                  <p>Connect your wallet to claim</p>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
-  )
+  );
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,13 +1,66 @@
-import Head from 'next/head'
+import Head from "next/head";
+import {
+  useAddress,
+  useContract,
+  useOwnedNFTs,
+  ThirdwebNftMedia,
+} from "@thirdweb-dev/react";
+
+import { TEST_CONTRACT_ADDRESS } from "@/config";
 
 export default function Genesis() {
+  const address = useAddress();
+
+  const truncateAddress = (address) => {
+    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  };
+
+  const { contract } = useContract(TEST_CONTRACT_ADDRESS);
+
+  const { data: ownedNFTs, isLoading: isOwnedNFTsLoading } = useOwnedNFTs(
+    contract,
+    address
+  );
+
   return (
     <>
       <div className="ui-screen">
-        Mint
-
-        <div className="placeholder"></div>
+        Collection
+        <div className="placeholder">
+          {address ? (
+            <div>
+              <div>
+                <h1>Profile</h1>
+                <p>Wallet Address: {truncateAddress(address) || ""}</p>
+              </div>
+              <hr />
+              <div>
+                <h3>My NFTs:</h3>
+                <div className="profile__grid">
+                  {!isOwnedNFTsLoading ? (
+                    ownedNFTs?.length > 0 ? (
+                      ownedNFTs?.map((nft) => (
+                        <div key={nft.metadata.id} className="profile__card">
+                          <ThirdwebNftMedia metadata={nft.metadata} />
+                          <h3>{nft.metadata.name}</h3>
+                        </div>
+                      ))
+                    ) : (
+                      <p>No NFTs owned.</p>
+                    )
+                  ) : (
+                    <p>Loading...</p>
+                  )}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <p>Plase connect your wallet</p>
+            </div>
+          )}
+        </div>
       </div>
     </>
-  )
+  );
 }
