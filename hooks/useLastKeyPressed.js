91338387f963139a839f9dde97cf1273b790caaf
mikepro4 <mikhailpro4@gmail.com>
2023-12-01 18:15:03 -0500

    ok cool

--- hooks/useLastKeyPressed.js
+++ hooks/useLastKeyPressed.js
@@ -29,37 +29,52 @@ const useLastKeyPressed = (dispatch, app, keyboard) => {
 
     const determineAction = (key) => {
         if (paramsData?.inputs && paramsData?.inputs[key] && paramsData?.inputs[key].param) {
-            if (paramsData?.inputs[key].paramType) {
-                if (paramsData?.inputs[key].paramType == "nextSwitch" || paramsData?.inputs[key].paramType == "prevSwitch") {
+            const inputConfig = paramsData?.inputs[key];
+            if (inputConfig && inputConfig.param) {
+                const currentParamValue = paramsValues[inputConfig.param];
+                let nextValue;
     
-                    const inputConfig = paramsData?.inputs[key];
-                    if (inputConfig && inputConfig.param) {
-                        const currentParamValue = paramsValues[inputConfig.param];
+                switch (inputConfig.paramType) {
+                    case "nextSwitch":
+                    case "prevSwitch":
                         const paramOptions = paramsData.params.find(p => p.value === inputConfig.param)?.enumParameters;
-    
                         if (paramOptions) {
                             const currentIndex = paramOptions.findIndex(option => option.value === currentParamValue);
                             let finalIndex = 0;
-    
-                            if (paramsData?.inputs[key].paramType == "nextSwitch") {
+                            if (inputConfig.paramType === "nextSwitch") {
                                 finalIndex = (currentIndex + 1) % paramOptions.length;
-                            } else if (paramsData?.inputs[key].paramType == "prevSwitch") {
-                                // Ensure the index wraps around correctly when it goes negative
+                            } else if (inputConfig.paramType === "prevSwitch") {
                                 finalIndex = (currentIndex - 1 + paramOptions.length) % paramOptions.length;
                             }
-    
-                            const nextValue = paramOptions[finalIndex].value;
-    
-                            return dispatch(toggleParamsValues({
-                                ...paramsValues,
-                                [inputConfig.param]: nextValue
-                            }));
+                            nextValue = paramOptions[finalIndex].value;
                         }
-                    }
+                        break;
+                    case "increment":
+                    case "decrement":
+                        const stepValue = inputConfig.paramStepValue || 1; // Default step is 1 if not specified
+                        const valueAsNumber = Number(currentParamValue);
+                        if (!isNaN(valueAsNumber)) {
+                            nextValue = inputConfig.paramType === "increment"
+                                ? (valueAsNumber + Number(stepValue))
+                                : (valueAsNumber - Number(stepValue));
+                        }
+                        break;
+                    default:
+                        // Handle other paramTypes or throw an error
+                        console.error(`Unhandled paramType: ${inputConfig.paramType}`);
+                        return;
+                }
+    
+                if (nextValue !== undefined) {
+                    return dispatch(toggleParamsValues({
+                        ...paramsValues,
+                        [inputConfig.param]: nextValue
+                    }));
                 }
             }
         }
     };
+    
 
     if (lastKey) {
         determineAction(lastKey); // Function to determine action based on key
