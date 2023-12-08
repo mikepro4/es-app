d2dd8abe978e5ca04e182b914da47893c8c0083b
mikepro4 <mikhailpro4@gmail.com>
2023-12-08 03:23:17 -0500

    ok kind of

--- components/generator/index.js
+++ components/generator/index.js
@@ -8,27 +8,123 @@ import ParamSwitch from "@/components/paramSwitch";
 import { toggleDrawer, generatorSearch, updateCollection } from "@/redux";
 import { set } from "lodash";
 
+import { 
+    toggleParamsValues,
+    generatorItem
+} from "@/redux";
+
 function AppSettings(props) {
     const [loading, setLoading] = useState(false);
     const app = useSelector((state) => state.app);
+    const player = useSelector((state) => state.audioPlayer);
     const router = useRouter();
     const [generator, setGenerator] = useState();
     const dispatch = useDispatch();
     const [generators, setGenerators] = useState([]);
-    const [currentIteration, setCurrentIteration] = useState(1);
     const [status, setStatus] = useState("stopped");
     const [timeInterval, setTimeInterval] = useState(null);
+    const [fullGenerator, setFullGenerator] = useState({});
+    const currentIterationRef = useRef(0);
+    const [prevPlayerIsPlaying, setPrevPlayerIsPlaying] = useState(false);
+    const [internalPlay, setInternalPlay] = useState(false)
 
+    const resetAnimation = () => {
+        setStatus("stopped");
+        currentIterationRef.current = 0
+        clearInterval(timeInterval);
+        setTimeInterval(null);
+        dispatch(toggleParamsValues(app.playerData.params));
+    }
 
 
     useEffect(() => {
-        searchGenerators()
+        return () => {
+            if (timeInterval) {
+                clearInterval(timeInterval);
+            }
+        };
+    }, [timeInterval]);
 
-        // dispatch(toggleDrawer({
-        //     drawerOpen: true,
-        //     drawerType: "generator-settings",
-        //     drawerData: props.item,
-        // }));
+    useEffect(() => {
+        if (player.isPlaying && status !== "play") {
+            if(!internalPlay) {
+                setStatus("play");
+                // If there's no interval set, start one
+                if (!timeInterval) {
+                    const intervalId = setInterval(() => {
+                        // Update the current iteration
+                        currentIterationRef.current += 1;
+                        // Perform the update values action
+                        updateValues();
+                    }, fullGenerator.params.iterationGap);
+                    // Store the interval ID so it can be cleared later
+                    setTimeInterval(intervalId);
+                }
+            } else {
+                setInternalPlay(false)
+            }
+        } else if (!player.isPlaying && status == "play" && !internalPlay) {
+            setStatus("paused");
+            setInternalPlay(false)
+            // If the player is not playing, clear the interval
+            if (timeInterval) {
+                clearInterval(timeInterval);
+                setTimeInterval(null);
+            }
+        }
+        // Update the previous player playing state
+        setPrevPlayerIsPlaying(player.isPlaying);
+        // Cleanup interval on unmount or if player.isPlaying changes
+        return () => {
+            if (timeInterval) {
+                clearInterval(timeInterval);
+            }
+        };
+    }, [player.isPlaying, status, timeInterval, updateValues, internalPlay]);
+ 
+
+    useEffect(() => {
+        dispatch(generatorItem({
+            id: generator,
+            callback: (data) => {
+                setFullGenerator(data)
+                resetAnimation()
+            }
+        }))
+    }, [generator])
+
+
+    const generateValue = (values, item, i) => {
+        if(item.paramType == "step") {
+
+            return Number(values[item.paramName]) + Number(item.stepAmount)*i
+        }
+    }
+
+    const updateValues = useCallback(() => {
+        if (fullGenerator) {
+            const paramsObject = fullGenerator?.params?.list.reduce((acc, item, index) => {
+                acc[item.paramName] = generateValue(app.playerData.params, item, currentIterationRef.current);
+                return acc;
+            }, {});
+    
+            let newParams = {
+                ...app.playerData.params,
+                ...paramsObject
+            };
+            dispatch(toggleParamsValues(newParams));
+            console.log(newParams);
+        }
+    }, [fullGenerator, app.playerData, dispatch, currentIterationRef.current]); 
+
+    // useEffect(() => {
+    //     if(currentIterationRef.current = 0) {
+    //         updateValues()
+    //     }
+    // }, [currentIterationRef.current]);
+
+    useEffect(() => {
+        searchGenerators()
 
         return () => {
             
@@ -38,19 +134,10 @@ function AppSettings(props) {
     useEffect(() => {
         if(app.updateCollection ) {
             searchGenerators()
-
-            // dispatch(toggleDrawer({
-            //     drawerOpen: true,
-            //     drawerType: "generator-settings",
-            //     drawerData: props.item,
-            // }));
-
             dispatch(updateCollection(false))
         }
        
-
         return () => {
-            
         };
     }, [app.updateCollection]); 
 
@@ -64,39 +151,39 @@ function AppSettings(props) {
                 order: 1,
 
                 callback: (data) => {
-                    let finalOptinos = data.all.map((option) => {
+                    let finalOptions = data.all.map((option) => {
                         return {
                             value: option._id,
                             label: option.name,
                         };
                     });
-                    setGenerators(finalOptinos);
-                    setGenerator(finalOptinos[0]?.value)
-                    // setTimestamp(Date.now())
-                    // if(finalOptinos[0]?.value && doSelect) {
-                    //     setTimeout(() => {
-                    //         selectGenerator(finalOptinos[0].value)
-                    //     }, 100)
-                    // }
-                   
+                    setGenerators(finalOptions);
+                    setGenerator(finalOptions[0]?.value)
+                    setFullGenerator(data.all[0])
                 },
             })
         );
     }
 
-    const togglePlay = () => {
+    const togglePlay = useCallback(() => {
         if (status !== "play") {
+            setInternalPlay(true)
             setStatus("play");
+            
             const intervalId = setInterval(() => {
-                setCurrentIteration(prevCurrentIteration => prevCurrentIteration + 1);
-            }, 1);
+                currentIterationRef.current = currentIterationRef.current + 1;
+                updateValues();
+            }, fullGenerator.params.iterationGap);
             setTimeInterval(intervalId);
         } else {
-            setStatus("stopped");
-            clearInterval(timeInterval);
-            setTimeInterval(null);
+            setInternalPlay(false)
+            setStatus("paused");
+            if (timeInterval) {
+                clearInterval(timeInterval);
+                setTimeInterval(null);
+            }
         }
-    };
+    }, [status, timeInterval, updateValues]); 
 
     let icon
 
@@ -148,10 +235,7 @@ function AppSettings(props) {
                         small={true}
                         offset={0.5}
                         onClick={() => {
-                            setStatus("stopped");
-                            setCurrentIteration(1);
-                            clearInterval(timeInterval);
-                            setTimeInterval(null);
+                            resetAnimation()
                         }}
                     />
                 </li>
@@ -163,19 +247,20 @@ function AppSettings(props) {
                         small={true}
                         onClick={() => {
                             let prevIteration
-                            if(currentIteration > 1) {
-                                prevIteration = currentIteration - 1
+                            if(currentIterationRef.current > 1) {
+                                prevIteration = currentIterationRef.current - 1
                             } else{
                                 prevIteration = 1
                             }
-                            setCurrentIteration(prevIteration);
+                            currentIterationRef.current = prevIteration
+                            updateValues()
                         }}
                     />
                 </li>
 
                 <li>
                     <div className="current-iteration">
-                        {currentIteration}
+                        {currentIterationRef.current}
                     </div>
                 </li>
 
@@ -187,7 +272,9 @@ function AppSettings(props) {
                         small={true}
                         offset={1}
                         onClick={() => {
-                            setCurrentIteration(currentIteration + 1);
+                            currentIterationRef.current = currentIterationRef.current + 1
+                            updateValues()
+                            // setCurrentIteration(currentIteration + 1);
                         }}
                     />
                 </li>
